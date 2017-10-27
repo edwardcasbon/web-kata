093a0057e712681f22e34d38263c0df9a75f23d0
Edward Casbon <edward@edwardcasbon.co.uk>
2017-10-27 11:23:14 +0100

    Kata 5

--- app5/app/src/App.js
+++ app5/app/src/App.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
-import data from './data.js'
+import 'whatwg-fetch'
 
 import ProductMenu from './ProductMenu.js'
 import ProductContainer from './ProductContainer.js'
+import ProductForm from './ProductForm.js'
 import './App.css'
 
 class App extends Component {
 
   constructor(props) {
     super(props)
-    this.state = { products: data.products }
+    this.state = { products: [] }
+    this.removeProduct = this.removeProduct.bind(this)
+    this.addProduct = this.addProduct.bind(this)
+
+    this.doApi({
+      url: '/api/products/get'
+    })
+  }
+
+  // Utility function to consume the API.
+  doApi(config) {
+    fetch(config.url, {
+      method: config.method || 'GET',
+      body: config.body || null,
+      headers: config.headers || {}
+    }).then(response => {
+      return response.json()
+    }).then(json => {
+      this.setState({products: json})
+    })
+  }
+
+  removeProduct(product) {
+    this.doApi({
+      url: `/api/products/delete/${product.name}`,
+      method: 'DELETE'
+    })
+  }
+
+  addProduct(product) {
+    const p = {
+      name: product.name,
+      description: product.description
+    }
+
+    this.doApi({
+      url: '/api/products/add',
+      method: 'POST',
+      body: JSON.stringify(p),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
   }
 
   render() {
@@ -18,9 +61,9 @@ class App extends Component {
       <div className="App-header">
         <h2>Kata 5 - Interaction with backend server through REST API calls</h2>
       </div>
-      <div className='products-add-product'>add product here</div>
+      <div className='products-add-product'><ProductForm onSubmit={this.addProduct}/></div>
       <div className='products-container'>
-        <ProductMenu products={this.state.products} />
+        <ProductMenu products={this.state.products} remove={this.removeProduct} />
         <Route exact path='/products/:productName' component={
           props => <ProductContainer {...props} products={this.state.products} />
         } />
--- app5/app/src/ProductMenu.js
+++ app5/app/src/ProductMenu.js
@@ -3,10 +3,22 @@ import './ProductMenu.css'
 import { Link } from 'react-router-dom'
 
 class ProductItem extends Component {
+  constructor(props) {
+    super(props)
+    this.remove = this.remove.bind(this)
+  }
+
+  remove() {
+    this.props.remove(this.props.product)
+  }
+
   render() {
     const name = this.props.product.name
     return <div className='product-item'>
-      <div className='name'><Link to={'/products/' + name}>{name}</Link></div>
+      <div className='name'>
+        <Link to={'/products/' + name}>{name}</Link>
+        <button onClick={this.remove}>Remove</button>
+      </div>
     </div>
   }
 }
@@ -15,7 +27,7 @@ class ProductMenu extends Component {
   render() {
     return <div className='product-menu'>
       {this.props.products.map(
-        (p, i) => <ProductItem product={p} key={'product-' + i} />
+        (p, i) => <ProductItem product={p} key={'product-' + i} remove={this.props.remove} />
       )}
     </div>
   }
