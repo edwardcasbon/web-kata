0b1889de931467bde7962c7409d0b56ea06433e0
Edward Casbon <edward@edwardcasbon.co.uk>
2018-02-20 15:42:21 +0000

    Created product action types.

--- app6/app/src/modules/products.js
+++ app6/app/src/modules/products.js
@@ -1,5 +1,45 @@
+export const PRODUCTS_REQUESTED = 'products/PRODUCTS_REQUESTED'
+export const PRODUCTS_COMPLETED = 'products/PRODUCTS_COMPLETED'
+export const PRODUCT_ADD_REQUESTED = 'products/PRODUCT_ADD_REQUESTED'
+export const PRODUCT_ADD_COMPLETED = 'products/PRODUCT_ADD_COMPLETED'
+export const PRODUCT_REMOVE_REQUESTED = 'products/PRODUCT_REMOVE_REQUESTED'
+export const PRODUCT_REMOVE_COMPLETED = 'products/PRODUCT_REMOVE_COMPLETED'
+
 const initialState = {}
 
 export default ( state = initialState, action ) => {
-    return state
+    switch (action.type) {
+        case PRODUCTS_REQUESTED:
+            return {
+                ...state
+            }
+
+        case PRODUCTS_COMPLETED:
+            return {
+                ...state
+            }
+
+        case PRODUCT_ADD_REQUESTED:
+            return {
+                ...state
+            }
+
+        case PRODUCT_ADD_COMPLETED:
+            return {
+                ...state
+            }
+
+        case PRODUCT_REMOVE_REQUESTED:
+            return {
+                ...state
+            }
+
+        case PRODUCT_REMOVE_COMPLETED:
+            return {
+                ...state
+            }
+
+        default:
+            return state
+    }
 }
